a28851e4846f5e59dbc359c1e0ef18bbec969a95
Apurba Biswas <apurba10024@gmail.com>
2018-09-10 14:15:19 +0600

    1.0 Added Initial Design For MainActivity

---
+++ app/src/main/java/com/example/apurba/test/courseinfo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.apurba.test.courseinfo;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewGroup rootHolder = findViewById(R.id.roo_holder);
+        View header = findViewById(R.id.header);
+        ViewGroup courseStateHolder = findViewById(R.id.course_holder);
+        ImageView newCourseImageView = findViewById(R.id.new_course_image);
+        TextView newCourseTextView = findViewById(R.id.new_course_text);
+        setClickListener(courseStateHolder,
+                newCourseImageView,
+                newCourseTextView);
+
+        ImageView runningCourseImageView = findViewById(R.id.running_course_image);
+        TextView runningCourseTextView = findViewById(R.id.running_course_text);
+        setClickListener(courseStateHolder,
+                runningCourseImageView,
+                runningCourseTextView);
+
+        ImageView completeCourseImageView = findViewById(R.id.complete_course_image);
+        TextView completeCourseTextView = findViewById(R.id.complete_course_text);
+        setClickListener(courseStateHolder,
+                completeCourseImageView,
+                completeCourseTextView);
+
+        setClickListener(rootHolder, header, courseStateHolder);
+
+    }
+
+    private void setClickListener(final ViewGroup transitionsContainer,
+                                  final View imageView,
+                                  final View textView){
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View view) {
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                if (textView.getVisibility() == View.GONE){
+                    textView.setVisibility(View.VISIBLE);
+                }else{
+                    textView.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
