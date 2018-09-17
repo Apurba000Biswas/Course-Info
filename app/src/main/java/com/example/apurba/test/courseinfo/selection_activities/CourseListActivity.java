db122c375bcc0e6034f38994d2800d68518a1fd0
Apurba Biswas <apurba10024@gmail.com>
2018-09-17 22:47:32 +0600

    2.5 Added Database Query for List view Pupulation in CourseListActivity

--- app/src/main/java/com/example/apurba/test/courseinfo/selection_activities/CourseListActivity.java
+++ app/src/main/java/com/example/apurba/test/courseinfo/selection_activities/CourseListActivity.java
@@ -1,20 +1,29 @@
 package com.example.apurba.test.courseinfo.selection_activities;
 
-
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
-
 import com.example.apurba.test.courseinfo.R;
+import com.example.apurba.test.courseinfo.database.CourseDatabaseContract.*;
 
 public class CourseListActivity extends AppCompatActivity {
+
+    private int status;
+    private courseCursorAdapter mCursorAdapter;
+    private static final int COURSE_LOADER_ID = 0;
+
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
@@ -24,14 +33,28 @@ public class CourseListActivity extends AppCompatActivity {
 
         int running = getIntent().getIntExtra("running",0);
         int complete = getIntent().getIntExtra("complete", 0);
+        int unknown = getIntent().getIntExtra("unknown", 0);
         if (running != 0){
             setCustomTittle(getResources().getString(R.string.tittle_running_course)
                     , getResources().getString(R.string.empty_running_course_state));
+            status = CourseEntry.STATUS_RUNNING;
         }
         if (complete != 0){
             setCustomTittle(getResources().getString(R.string.tittle_complete_course)
                     , getResources().getString(R.string.empty_complete_course_state));
+            status = CourseEntry.STATUS_COMPLETE;
         }
+        if (unknown != 0){
+            status = CourseEntry.STATUS_UNKNOWN;
+        }
+
+        ListView courseListView = findViewById(R.id.course_list_view);
+        mCursorAdapter = new courseCursorAdapter(this, null);
+        courseListView.setAdapter(mCursorAdapter);
+
+        View emptyView = findViewById(R.id.empty_state_view);
+        courseListView.setEmptyView(emptyView);
+        getSupportLoaderManager().initLoader(COURSE_LOADER_ID, null, courseLoaderlistener);
 
     }
 
@@ -47,5 +70,39 @@ public class CourseListActivity extends AppCompatActivity {
         TextView emptyState = findViewById(R.id.empty_state_text_view);
         emptyState.setText(emptyStateMessage);
     }
+
+
+
+
+
+    private LoaderManager.LoaderCallbacks<Cursor> courseLoaderlistener = new LoaderManager.LoaderCallbacks<Cursor>() {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+            String[] projection = {CourseEntry._ID
+                    , CourseEntry.COLUMN_COURSE_NAME
+                    , CourseEntry.COLUMN_INSTRUCTOR_ID
+                    , CourseEntry.COLUMN_STATUS
+                    , CourseEntry.COLUMN_START_TIME
+                    , CourseEntry.COLUMN_END_TIME};
+            String selection = CourseEntry.COLUMN_STATUS + " = " + status;
+            return new CursorLoader(CourseListActivity.this,
+                    CourseEntry.CONTENT_URI,
+                    projection,
+                    selection,
+                    null,
+                    null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            mCursorAdapter.swapCursor(data);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            mCursorAdapter.swapCursor(null);
+        }
+    };
 
 }
---
+++ app/src/main/java/com/example/apurba/test/courseinfo/selection_activities/courseCursorAdapter.java
@@ -0,0 +1,77 @@
+package com.example.apurba.test.courseinfo.selection_activities;
+
+/*
+ * Created by Apurba on 9/17/2018.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.apurba.test.courseinfo.R;
+import com.example.apurba.test.courseinfo.database.CourseDatabaseContract.*;
+
+public class courseCursorAdapter extends CursorAdapter{
+
+    public courseCursorAdapter(Context context, Cursor c){
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.course_list_item
+                , viewGroup
+                , false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView courseCodeText = view.findViewById(R.id.course_code);
+        TextView courseNameText = view.findViewById(R.id.course_name);
+        TextView instructorIdText = view.findViewById(R.id.instructor_id);
+        TextView statusText = view.findViewById(R.id.status);
+        TextView startTime = view.findViewById(R.id.start_time);
+        TextView endTime = view.findViewById(R.id.end_time);
+
+        int courseCodeColumnIndex = cursor.getColumnIndex(CourseEntry._ID);
+        int courseNameColumnIndex = cursor.getColumnIndex(CourseEntry.COLUMN_COURSE_NAME);
+        int instructorIdColumnIndex = cursor.getColumnIndex(CourseEntry.COLUMN_INSTRUCTOR_ID);
+        int statusColumnIndex = cursor.getColumnIndex(CourseEntry.COLUMN_STATUS);
+        int startTimeColumnIndex = cursor.getColumnIndex(CourseEntry.COLUMN_START_TIME);
+        int endTimeColumnIndex = cursor.getColumnIndex(CourseEntry.COLUMN_END_TIME);
+
+        String courseCode = cursor.getString(courseCodeColumnIndex);
+        String courseName = cursor.getString(courseNameColumnIndex);
+        String instructorID = cursor.getString(instructorIdColumnIndex);
+        int status = cursor.getInt(statusColumnIndex);
+        String startDate = cursor.getString(startTimeColumnIndex);
+        String endDate = cursor.getString(endTimeColumnIndex);
+
+        courseCodeText.setText(courseCode);
+        courseNameText.setText(courseName);
+        instructorIdText.setText(instructorID);
+        statusText.setText(getStatus(status));
+        startTime.setText(startDate);
+        endTime.setText(endDate);
+
+        Animation animation = AnimationUtils.loadAnimation(context, R.anim.scale);
+        view.startAnimation(animation);
+
+    }
+
+    private String getStatus(int status){
+        if (status == CourseEntry.STATUS_RUNNING){
+            return "Running";
+        }else if (status == CourseEntry.STATUS_COMPLETE){
+            return "Completed";
+        }else{
+            return "Unknown";
+        }
+    }
+}
